c4f17ca9b4758412c9f499ed1e4fca62985e7ef9
MaithilBoghara <bogharamaithil3@gmail.com>
2022-06-11 09:37:04 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React from "react";
 import Header from "./Components/Header/Header";
 import Home from "./Container/Home";
 import Footer from "./Components/Footer/Footer";
-
+import { Route, Switch } from "react-router-dom";
+import Departments from "./Container/Departments";
+import Doctor from "./Container/Doctor";
+import About from "./Container/About";
+import Contact from "./Container/Contact";
+import Form from "./Container/Form";
 
 function App() {
   return (
    <>
    <Header />
-   <Home />
+   <Switch>
+     <Route path={"/"} exact component={Home} />
+     <Route path={"/department"} exact component={Departments} />
+     <Route path={"/doctor"} exact component={Doctor} />
+     <Route path={"/about"} exact component={About} />
+     <Route path={"/contact"} exact component={Contact} />
+     <Route path={"/form"} exact component={Form} />
+   </Switch>
+
+   
+
    <Footer />
    </>
   );
